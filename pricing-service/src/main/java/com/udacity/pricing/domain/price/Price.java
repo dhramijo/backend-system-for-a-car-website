7f15d89e692a3a7fae2416ffb21a6b4ca1ffa922
dhramijo <dhrami86@yahoo.it>
2020-08-31 20:39:54 +0100

    Adding Lombok dependency in pricing service

--- pricing-service/src/main/java/com/udacity/pricing/domain/price/Price.java
+++ pricing-service/src/main/java/com/udacity/pricing/domain/price/Price.java
@@ -1,10 +1,13 @@
 package com.udacity.pricing.domain.price;
 
+import lombok.Data;
+
 import java.math.BigDecimal;
 
 /**
  * Represents the price of a given vehicle, including currency.
  */
+@Data
 public class Price {
 
     private String currency;
@@ -17,30 +20,6 @@ public class Price {
     public Price(String currency, BigDecimal price, Long vehicleId) {
         this.currency = currency;
         this.price = price;
-        this.vehicleId = vehicleId;
-    }
-
-    public String getCurrency() {
-        return currency;
-    }
-
-    public void setCurrency(String currency) {
-        this.currency = currency;
-    }
-
-    public BigDecimal getPrice() {
-        return price;
-    }
-
-    public void setPrice(BigDecimal price) {
-        this.price = price;
-    }
-
-    public Long getVehicleId() {
-        return vehicleId;
-    }
-
-    public void setVehicleId(Long vehicleId) {
         this.vehicleId = vehicleId;
     }
 }
