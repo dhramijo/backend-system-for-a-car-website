e1ce18fba9159ae62ed818bf957f32ec9c865b8c
dhramijo <dhrami86@yahoo.it>
2020-09-05 23:14:48 +0100

    Implemented the car service create, read, update and delete operations.

--- vehicles-api/src/main/java/com/udacity/vehicles/service/CarService.java
+++ vehicles-api/src/main/java/com/udacity/vehicles/service/CarService.java
@@ -2,6 +2,7 @@ package com.udacity.vehicles.service;
 
 import com.udacity.vehicles.client.maps.MapsClient;
 import com.udacity.vehicles.client.prices.PriceClient;
+import com.udacity.vehicles.domain.Location;
 import com.udacity.vehicles.domain.car.Car;
 import com.udacity.vehicles.domain.car.CarRepository;
 import java.util.List;
@@ -48,7 +49,7 @@ public class CarService {
          *   If it does not exist, throw a CarNotFoundException
          *   Remove the below code as part of your implementation.
          */
-        Car car = new Car();
+        Car car = repository.findById(id).orElseThrow(() -> new CarNotFoundException());
 
         /**
          * TODO: Use the Pricing Web client you create in `VehiclesApiApplication`
@@ -57,7 +58,7 @@ public class CarService {
          * Note: The car class file uses @transient, meaning you will need to call
          *   the pricing service each time to get the price.
          */
-
+        car.setPrice(priceClient.getPrice(id));
 
         /**
          * TODO: Use the Maps Web client you create in `VehiclesApiApplication`
@@ -67,7 +68,8 @@ public class CarService {
          * Note: The Location class file also uses @transient for the address,
          * meaning the Maps service needs to be called each time for the address.
          */
-
+        Location carAddress = mapsClient.getAddress(car.getLocation());
+        car.setLocation(carAddress);
 
         return car;
     }
@@ -99,12 +101,12 @@ public class CarService {
          * TODO: Find the car by ID from the `repository` if it exists.
          *   If it does not exist, throw a CarNotFoundException
          */
-
+        Car car = repository.findById(id).orElseThrow(() -> new CarNotFoundException());
 
         /**
          * TODO: Delete the car from the repository.
          */
-
+        repository.delete(car);
 
     }
 }
